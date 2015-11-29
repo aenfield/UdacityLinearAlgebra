98437af670f6e1362e5fac92cdb598b9259065b4
Andrew Enfield <aenfield@gmail.com>
2015-11-28 17:36:33 -0800

    Finished subtraction and scalar multiplication implementation, and used to answer course questions.

--- vector-test.py
+++ vector-test.py
@@ -17,6 +17,34 @@ class AdditionTest(unittest.TestCase):
         self.assertEqual(Vector([-4,-3]), v1+v2)
 
 
+class SubtractionTest(unittest.TestCase):
+
+    def testSubtraction(self):
+        v1 = Vector([1,4])
+        v2 = Vector([3,2])
+
+        self.assertEqual(Vector([-2,2]), v1-v2)
+
+
+class ScalarMultiplyTest(unittest.TestCase):
+
+    def testPositiveScalarMultiplication(self):
+        v1 = Vector([1,2])
+
+        self.assertEqual(Vector([3,6]), v1*3)
+
+    def testNegativeScalarMultiplication(self):
+        v1 = Vector([2,4])
+
+        self.assertEqual(Vector([-4,-8]), v1*-2)
+
+    # here we test 3*v instead of v*3 (as above)
+    def testScalarMultiplicationWithScalarFirst(self):
+        v1 = Vector([1,2])
+
+        self.assertEqual(Vector([3,6]), 3*v1)
+
+
 
 
 
--- vector.py
+++ vector.py
@@ -25,3 +25,25 @@ class Vector(object):
         # when applied to tuples, + concatenates, so we need to
         # zip the two tuples together and add the results
         return Vector([x+y for (x,y) in zip(self.coordinates, v.coordinates)])
+
+    def __sub__(self, v):
+        return Vector([x-y for (x,y) in zip(self.coordinates, v.coordinates)])
+        # would it be better to define a unary negative and then implement this as self + (-(v))? reduces duplication...
+
+    # support vector * scalar (for ex, v*3)
+    def __mul__(self, v):
+        return Vector([v*x for x in self.coordinates])
+
+    # we overload __rmul__ to support scalar * vector operations (for ex, 3*v)
+    def __rmul__(self, v):
+        return self * v  # implement by calling __mul__ here
+
+
+
+
+
+
+if __name__ == '__main__':
+    print(Vector([8.218,-9.341]) + Vector([-1.129,2.111]))
+    print(Vector([7.119,8.215]) - Vector([-8.223,0.878]))
+    print(7.41 * Vector([1.671,-1.012,-0.318]))
