c0790c76ae191eddb5a0d169e9ccc9c106ff94d7
Andrew Enfield <andrewe@microsoft.com>
2015-12-07 10:14:55 -0800

    Fixed issue using functions to answer class questions - user error as I'd forgotten to pass a vector and was just passing a sequence.

--- plane.py
+++ plane.py
@@ -123,14 +123,14 @@ if __name__ == '__main__':
     # here we're using the implemented functions to output the results of the
     # questions asked by the course
 
-    p1 = Plane([-0.412,3.806,0.728], -3.46)
-    p2 = Plane([1.03,-9.515,-1.82], 8.65)
-    print(p1.parallelTo(p2), p1 == p2)
+    p1 = Plane(Vector([-0.412,3.806,0.728]), -3.46)
+    p2 = Plane(Vector([1.03,-9.515,-1.82]), 8.65)
+    print(p1 == p2, p1.parallelTo(p2))
 
-    p1 = Plane([2.611,5.528,0.283], 4.6)
-    p2 = Plane([7.715,8.306,5.342], 3.76)
-    print(p1.parallelTo(p2), p1 == p2)
+    p1 = Plane(Vector([2.611,5.528,0.283]), 4.6)
+    p2 = Plane(Vector([7.715,8.306,5.342]), 3.76)
+    print(p1 == p2, p1.parallelTo(p2))
 
-    p1 = Plane([-7.926,8.625,-7.212], -7.952)
-    p2 = Plane([-2.642,2.875,-2.404], -2.443)
-    print(p1.parallelTo(p2), p1 == p2)
+    p1 = Plane(Vector([-7.926,8.625,-7.212]), -7.952)
+    p2 = Plane(Vector([-2.642,2.875,-2.404]), -2.443)
+    print(p1 == p2, p1.parallelTo(p2))
